9ba658f26a5e062b7638236c9fcbbaaeb87ce8d9
Jared Draper <jared@freshlycutwax.com>
2025-03-30 02:25:36 -0500

    documentation styling

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../src/'))
 # -- Project information -----------------------------------------------------
 
 project = 'KTMM'
-copyright = '2025, '
+copyright = '2025, FreshlyCutWax'
 author = 'Jared Draper, Camilo Weinert-Palomino, Grant Wilke, Joshua Borthick'
 
 # The full version, including alpha/beta/rc tags
@@ -43,6 +43,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+primary_domain = 'c'
+highlight_language = 'none'
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -51,7 +53,18 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'alabaster'
 
+html_theme_options = {
+    "pre_bg": "#3C3836",
+    "link":   "#D79921",
+    "sidebar_header": "#ebdbb2",
+    "sidebar_link":   "#D79921",
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# custom side bar templates ( see Sphinx documentation for more )
+html_sidebars = { '**': [ 'searchbox.html', 'globaltoc.html' ] }
+
